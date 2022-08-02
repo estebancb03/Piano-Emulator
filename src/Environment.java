6c3658e75bab7818a17fd6f47efbc7f243ff80d5
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-08-01 18:25:34 -0600

    Environment class and its most important properties are created

---
+++ src/Environment.java
@@ -0,0 +1,17 @@
+/// @copyright 2022 Esteban Castañeda Blanco. All rights reserved
+/// This code is released under the GNU Public License version 3
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Environment extends JFrame {
+  /// Properties
+  private int rows;
+  private int columns;
+  private JButton btnStep;
+  private JButton btnStart;
+  private JButton btnPause;
+  private JButton btnReset;
+  private JPanel mainPanel;
+}
