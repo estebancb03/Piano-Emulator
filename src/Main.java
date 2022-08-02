40fdb2aa606b24e8f88df112f24536570494c19f
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-08-01 18:12:23 -0600

    main file is created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/// @copyright 2022 Esteban Castañeda Blanco. All rights reserved
+/// This code is released under the GNU Public License version 3
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+
+public class Main {
+  public static void main(String[] args) {
+      
+  }
+}
